dff1c7d9d50440fa838b9c4f1080da728fc3229b
Aman Agarwal <aman.agarwal@inmobi.com>
2019-09-24 20:49:56 +0530

    Setting base application

---
+++ ContactsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactsApp
+//
+//  Created by Aman Agarwal on 24/09/19.
+//  Copyright © 2019 Aman Agarwal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
